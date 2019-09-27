e8e0c1aaaa63c19f804990ad657cf6ad36a073b1
Matt Robinson <mrobinson@strava.com>
2019-09-26 17:03:42 -0700

    Initial Commit

---
+++ LocationApprovalTester/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocationApprovalTester
+//
+//  Created by Matt Robinson on 9/26/19.
+//  Copyright © 2019 Strava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
